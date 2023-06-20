d69c99db407625c9bd1a7227d939848c6b020279
Ben Knighton <131706686+BenKnighton@users.noreply.github.com>
2023-06-20 19:38:55 +0100

    Uploaded files

---
+++ repos/Voice Recognition/Voice Recognition/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace Voice_Recognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] {"hello", "print my name" });
+            GrammarBuilder gBuilder = new GrammarBuilder(); //tells the Speech recog what type of commands are going to be used 
+            gBuilder.Append(commands);
+            Grammar grammer = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammer); 
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "hello":
+                    MessageBox.Show("Hello Ben!");
+                    break;
+                case "print my name":
+                    richTextBox1.Text += "\nBen";
+                    break;
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;  
+        }
+    }
+}
